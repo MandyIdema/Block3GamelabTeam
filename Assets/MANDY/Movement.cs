058535329ced9d1690bbdeccaec86703e89ae076
Mandy Idema <m.e.idema@st.hanze.nl>
2023-03-15 12:19:02 +0100

    Changes
    - Imported TMP
    - Added nicknames to characters
    - Added random colors to character spawn
    - Fixed networking issues

--- Assets/MANDY/Movement.cs
+++ Assets/MANDY/Movement.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 
 public class Movement : NetworkBehaviour
 {
+
+    public TextMesh playerNameText;
+    public GameObject floatingInfo;
+
+    private Material playerMaterialClone;
+
     float speed = 0.1f;
     // Start is called before the first frame update
     void Start()
@@ -24,10 +30,47 @@ public class Movement : NetworkBehaviour
         {
             float moveHorizontal = Input.GetAxis("Horizontal");
             float moveVertical = Input.GetAxis("Vertical");
-            Vector3 movement = new Vector3(moveHorizontal * speed, moveVertical * speed, 0);
+            Vector3 movement = new Vector2(moveHorizontal * speed, moveVertical * speed);
             this.transform.position = transform.position + movement;
         }
 
+    }
+
+ 
+
+    [SyncVar(hook = nameof(OnNameChanged))]
+    public string playerName;
+
+    [SyncVar(hook = nameof(OnColorChanged))]
+    public Color playerColor = Color.white;
+
+    void OnNameChanged(string _Old, string _New)
+    {
+        playerNameText.text = playerName;
+    }
+
+    void OnColorChanged(Color _Old, Color _New)
+    {
+        playerNameText.color = _New;
+        playerMaterialClone = new Material(GetComponent<Renderer>().material);
+        playerMaterialClone.color = _New;
+        GetComponent<Renderer>().material = playerMaterialClone;
+    }
+
+    public void CmdSetupPlayer(string _name, Color _col)
+    {
+        // player info sent to server, then server updates sync vars which handles it on all clients
+        playerName = _name;
+        playerColor = _col;
+    }
+
+    public override void OnStartLocalPlayer()
+    {
+
+
+        string name = "Player" + Random.Range(100, 999);
+        Color color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+        CmdSetupPlayer(name, color);
     }
 }
 
