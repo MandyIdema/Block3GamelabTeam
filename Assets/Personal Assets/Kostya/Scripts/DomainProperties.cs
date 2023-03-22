866c616d03fd33acb4f2fc08d37b085b264185ea
Kostya Deputatov <kostyadeputatov@gmail.com>
2023-03-22 09:44:00 +0100

    - Another experiment
    BRUHHH

--- Assets/Personal Assets/Kostya/Scripts/DomainProperties.cs
+++ Assets/Personal Assets/Kostya/Scripts/DomainProperties.cs
@@ -83,19 +83,12 @@ public class DomainProperties : NetworkBehaviour
         }
 
     }
-
+    [Client]
     private void InitializeDomainMenu(bool activationStatus)
     {
         // Currently uses this structure to get the text object, might change later
-        // domainClassMenu.GetComponentInChildren<TextMeshProUGUI>().text = domainDesc.domainDescription[domainNumber];
-        // domainClassMenu.SetActive(activationStatus);
-
-        if (NetworkServer.active || NetworkClient.active)
-        {
-            domainClassMenu.GetComponentInChildren<TextMeshProUGUI>().text = domainDesc.domainDescription[domainNumber];
-            domainClassMenu.SetActive(activationStatus);
-        }
-
+        domainClassMenu.GetComponentInChildren<TextMeshProUGUI>().text = domainDesc.domainDescription[domainNumber];
+        domainClassMenu.SetActive(activationStatus);
     }
 
     // Currently not used
