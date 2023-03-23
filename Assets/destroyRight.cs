7e030f1bad9b6f6b46bc145a383f0684880fb328
Kostya Deputatov <kostyadeputatov@gmail.com>
2023-03-23 12:58:12 +0100

    Trying out Network Behaviour

--- Assets/destroyRight.cs
+++ Assets/destroyRight.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Mirror;
 
-public class destroyRight : MonoBehaviour
+public class destroyRight : NetworkBehaviour
 {
     private void OnCollisionStay2D(Collision2D collision)
     {
