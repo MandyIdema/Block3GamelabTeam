2fd5eb049961a16ef7bd44fe4df285690f2a0abb
Kirirgiri <95420741+Kirirgiri@users.noreply.github.com>
2023-03-23 12:03:08 +0100

    changes
    whatever

--- Assets/Personal Assets/Mandy/Movement.cs
+++ Assets/Personal Assets/Mandy/Movement.cs
@@ -16,7 +16,7 @@ public class Movement : NetworkBehaviour
 
     private Material playerMaterialClone;
 
-    float speed = 0.1f;
+    float speed = 0.01f;
     // Start is called before the first frame update
     void Start()
     {
