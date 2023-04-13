3dcb0df7a8c422deb7ee4adab74ab1ba6ede35a4
Mandy Idema <m.e.idema@st.hanze.nl>
2023-04-13 13:01:35 +0200

    Inserted new Font

--- Assets/Personal Assets/Mandy/QuestionScript.cs
+++ Assets/Personal Assets/Mandy/QuestionScript.cs
@@ -6,16 +6,19 @@ using TMPro;
 
 public class QuestionScript : MonoBehaviour
 {
+    //The text for feedback
     public TMP_Text awnserText;
 
 
     private void Start()
     {
+        //The text will be disabled if nothing is clicked yet
         awnserText.enabled = false;
     }
 
     public void wrongAwnser()
     {
+        //If the awnser is wrong, state in the feedback that it is wrong
         awnserText.enabled = true;
         awnserText.GetComponent<TextMeshProUGUI>().text = "Fout! probeer het nog een keer";
         Debug.Log("This is the wrong awnser");
@@ -23,6 +26,7 @@ public class QuestionScript : MonoBehaviour
 
     public void rightAwnser()
     {
+        //If the awnser is right, state in the feedback that it is right and close the panel
         awnserText.enabled = true;
         awnserText.GetComponent<TextMeshProUGUI>().text = "Goed!";
         Debug.Log("This is the right awnser");
@@ -31,6 +35,7 @@ public class QuestionScript : MonoBehaviour
 
     IEnumerator closeWindow()
     {
+        //After two seconds, close the panel
         Debug.Log("Closed window");
 
         yield return new WaitForSeconds(2);
