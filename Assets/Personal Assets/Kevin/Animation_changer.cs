2608128787d935f7042d190a620c69a38839c674
Mandy Idema <m.e.idema@st.hanze.nl>
2023-05-24 10:49:57 +0200

    Added changes

--- Assets/Personal Assets/Kevin/Animation_changer.cs
+++ Assets/Personal Assets/Kevin/Animation_changer.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Sprite_changer : MonoBehaviour
+public class Animation_changer : MonoBehaviour
 {
     public Sprite newSprite; 
 
@@ -11,17 +11,7 @@ public class Sprite_changer : MonoBehaviour
 
     private void Start()
     {
-        spriteRenderer = GetComponent<SpriteRenderer>();
-        originalSprite = spriteRenderer.sprite;
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == "Player")
-        {
-            spriteRenderer.sprite = newSprite;
-            StartCoroutine(ChangeBackAfterDelay(3f));
-        }
+        
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -29,7 +19,7 @@ public class Sprite_changer : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             spriteRenderer.sprite = newSprite;  
-            StartCoroutine(ChangeBackAfterDelay(1f));
+            StartCoroutine(ChangeBackAfterDelay(3f));
         }
     }
 
