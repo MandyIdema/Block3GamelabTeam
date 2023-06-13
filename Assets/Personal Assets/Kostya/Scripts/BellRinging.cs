771a3a50764fa9db9995ada69ac9c65bfdfe2be5
Kostya Deputatov <kostyadeputatov@gmail.com>
2023-06-13 14:01:30 +0200

    More updates

--- Assets/Personal Assets/Kostya/Scripts/BellRinging.cs
+++ Assets/Personal Assets/Kostya/Scripts/BellRinging.cs
@@ -4,35 +4,35 @@ using UnityEngine;
 
 public class BellRinging : MonoBehaviour
 {
-    public AudioSource bellSource;
-    public AudioClip clinkSound;
-    public GameObject questionUI;
-    void Start()
-    {
-        bellSource.clip = clinkSound;
-    }
+    //public AudioSource bellSource;
+    //public AudioClip clinkSound;
+    //public GameObject questionUI;
+    //void Start()
+    //{
+    //    bellSource.clip = clinkSound;
+    //}
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.CompareTag("Player"))
-        {
-            collision.gameObject.GetComponent<PlayerBehaviour>().currentInteractiveObject = gameObject;
-            collision.gameObject.GetComponent<PlayerBehaviour>().inInteractionRange = true;
-        }
-    }
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    if (collision.gameObject.CompareTag("Player"))
+    //    {
+    //        collision.gameObject.GetComponent<PlayerBehaviour>().currentInteractiveObject = gameObject;
+    //        collision.gameObject.GetComponent<PlayerBehaviour>().inInteractionRange = true;
+    //    }
+    //}
 
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.gameObject.CompareTag("Player"))
-        {
-            collision.gameObject.GetComponent<PlayerBehaviour>().currentInteractiveObject = null;
-            collision.gameObject.GetComponent<PlayerBehaviour>().inInteractionRange = false;
-        }
-    }
+    //private void OnTriggerExit2D(Collider2D collision)
+    //{
+    //    if (collision.gameObject.CompareTag("Player"))
+    //    {
+    //        collision.gameObject.GetComponent<PlayerBehaviour>().currentInteractiveObject = null;
+    //        collision.gameObject.GetComponent<PlayerBehaviour>().inInteractionRange = false;
+    //    }
+    //}
 
-    public void RingingBell()
-    {
-        Debug.Log("this is triggered");
-        bellSource.Play();
-    }
+    //public void RingingBell()
+    //{
+    //    Debug.Log("this is triggered");
+    //    bellSource.Play();
+    //}
 }
